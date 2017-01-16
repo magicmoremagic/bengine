f58db6bcbc889995584c4e5fb6b2840a24684b98
Ben Crist <ben@magicmoremagic.com>
2017-01-16 02:54:26 -0600

    added glbinding ext_lib

--- dep_modules/build.lua
+++ dep_modules/build.lua
@@ -1,12 +1,13 @@
 deps {
-   ext_include_dir 'glm'      { path 'glm/glm' },
-   ext_include_dir 'gsl'      { path 'gsl/gsl' },
-   ext_include_dir 'glfw'     { path 'glfw/include/GLFW' },
-   ext_include_dir 'pugixml'  { path 'pugixml/src' },
-   ext_include_dir 'catch'    { path 'catch/single_include' },
-   ext_include_dir 'lua' { },
-   ext_include_dir 'stb' { },
-   ext_include_dir 'zlib' { },
+   ext_include_dir 'glm'       { path 'glm/glm' },
+   ext_include_dir 'gsl'       { path 'gsl/gsl' },
+   ext_include_dir 'glbinding' { path 'glbinding/source/glbinding/include/glbinding' },
+   ext_include_dir 'glfw'      { path 'glfw/include/GLFW' },
+   ext_include_dir 'pugixml'   { path 'pugixml/src' },
+   ext_include_dir 'catch'     { path 'catch/single_include' },
+   ext_include_dir 'lua'       { },
+   ext_include_dir 'stb'       { },
+   ext_include_dir 'zlib'      { },
 
    ext_lib 'zlib-static' {
       path 'zlib',
@@ -19,10 +20,37 @@ deps {
       }
    },
 
+   ext_lib 'glbinding' {
+      path 'glbinding',
+      force_cxx,
+      include 'source/glbinding/include/glbinding',
+      src {
+         'source/glbinding/source/Binding_*.cpp',
+         pch_src 'source/glbinding/source/Binding_pch.cpp',
+         pch 'Binding_pch.h'
+      },
+      src {
+         'source/glbinding/source/gl/functions_*.cpp',
+         pch_src 'source/glbinding/source/Binding_pch.cpp',
+         pch '../Binding_pch.h'
+      },
+      src {
+         'source/glbinding/source/*.cpp',
+         'source/glbinding/source/gl/*.cpp',
+         exclude 'source/glbinding/source/Binding_*.cpp',
+         exclude 'source/glbinding/source/gl/functions_*.cpp'
+      },
+      define 'GLBINDING_STATIC_DEFINE',
+      toolchain 'vc_win' {
+         link 'opengl32'
+      }
+   },
+
    ext_lib 'glfw' {
       path 'glfw',
       force_c,
-      include { 'include', 'src' },
+      --include { 'include', 'src' },
+      include 'include',
       src {
          'src/context.c',
          'src/init.c',
@@ -37,7 +65,8 @@ deps {
             'src/wgl_*.c',
             'src/egl_*.c'
          },
-         define '_GLFW_WIN32'
+         define '_GLFW_WIN32',
+         link 'opengl32'
       }
    },
 
