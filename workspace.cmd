d67d6e5caa8d9e0fcd186cfcad90148fdfcaa2d0
Ben Crist <ben@magicmoremagic.com>
2017-03-12 12:13:15 -0500

    VS2017

--- workspace.cmd
+++ workspace.cmd
@@ -1,23 +1,28 @@
 @echo off
 pushd %~dp0
 
-if defined VSINSTALLDIR (
-   set _vcvarsall="%VSINSTALLDIR%VC\vcvarsall.bat"
-) else (
-   set _vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+for /f "usebackq tokens=*" %%i in (`"%~dp0bin\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath`) do (
+  set _vcvarsall="%%i\VC\Auxiliary\Build\vcvarsall.bat"
 )
 
 if not exist %_vcvarsall% (
-   set _vcvarsall="C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat"
+   if defined VSINSTALLDIR (
+      set _vcvarsall="%VSINSTALLDIR%VC\vcvarsall.bat"
+   ) else (
+      set _vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+   )
+
    if not exist %_vcvarsall% (
-      echo Failed to locate vcvarsall.bat!
-      pause
-      exit /b 1
+      set _vcvarsall="C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat"
+      if not exist %_vcvarsall% (
+         echo Failed to locate vcvarsall.bat!
+         pause
+         exit /b 1
+      )
    )
 )
 
 call %_vcvarsall% x64
 set PATH=%~dp0stage;%~dp0bin;%PATH%
-ninja init!
 cmd
 popd
