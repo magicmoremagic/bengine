47f0569498fbfdb8a80bf258ea18142fef309164
Ben Crist <ben@magicmoremagic.com>
2017-09-21 02:58:54 -0500

    Replacing glbinding with BGL

--- dep_modules/build.lua
+++ dep_modules/build.lua
@@ -1,8 +1,6 @@
 deps {
    ext_include_dir 'glm'       { path 'glm/glm' },
    ext_include_dir 'gsl'       { path 'gsl/gsl' },
-   ext_include_dir 'glbinding' { path 'glbinding/source/glbinding/include/glbinding' },
-   ext_include_dir 'globjects' { path 'globjects/source/globjects/include/globjects' },
    ext_include_dir 'glfw'      { path 'glfw/include/GLFW' },
    ext_include_dir 'pugixml'   { path 'pugixml/src' },
    ext_include_dir 'catch'     { path 'catch/single_include' },
@@ -37,47 +35,6 @@ deps {
       }
    },
 
-   ext_lib 'glbinding' {
-      path 'glbinding',
-      force_cxx,
-      include 'source/glbinding/include/glbinding',
-      src {
-         'source/glbinding/source/Binding_*.cpp',
-         pch_src 'source/glbinding/source/Binding_pch.cpp',
-         pch 'Binding_pch.h'
-      },
-      src {
-         'source/glbinding/source/gl/functions_*.cpp',
-         pch_src 'source/glbinding/source/Binding_pch.cpp',
-         pch '../Binding_pch.h'
-      },
-      src {
-         'source/glbinding/source/*.cpp',
-         'source/glbinding/source/gl/*.cpp',
-         exclude 'source/glbinding/source/Binding_*.cpp',
-         exclude 'source/glbinding/source/gl/functions_*.cpp'
-      },
-      define 'GLBINDING_STATIC_DEFINE',
-      export_define 'GLFW_INCLUDE_NONE',
-      toolchain 'vc_win' {
-         link 'opengl32'
-      }
-   },
-
-   ext_lib 'globjects' {
-      path 'globjects',
-      force_cxx,
-      include 'source/globjects/include/globjects',
-      src {
-         'source/globjects/source/*.cpp',
-         'source/globjects/source/base/*.cpp',
-         'source/globjects/source/implementations/*.cpp',
-         'source/globjects/source/registry/*.cpp'
-      },
-      define 'GLOBJECTS_STATIC_DEFINE',
-      link_project 'glbinding'
-   },
-
    ext_lib 'glfw' {
       path 'glfw',
       force_c,
@@ -90,6 +47,7 @@ deps {
          'src/vulkan.c',
          'src/window.c'
       },
+      export_define 'GLFW_INCLUDE_NONE',
       toolchain 'vc_win' {
          src {
             'src/win32_*.c',
